3c1227e946d93699b45e0c888ebf7e39be5a1c05
Apan Jain <apanjain3@gmail.com>
2020-12-23 17:13:55 +0530

    Filter routes and write data to respective files

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const baseUrl = 'http://0.0.0.0:5000/';
-axios.get(`${baseUrl}?rest_route=/wp/v2/`)
-.then(res=> {
+const baseUrl = "http://0.0.0.0:5000/?rest_route=";
+
+// Details of Routes
+
+axios
+  .get(`${baseUrl}/wp/v2/`)
+  .then((res) => {
     const data = res.data;
     const routes = data.routes;
-    const writeData = JSON.stringify(data,null,2);
-    fs.writeFileSync('response.json', writeData);
+    const writeData = JSON.stringify(data, null, 2);
+    fs.writeFileSync("details.result.json", writeData);
 
-})
-.catch((err)=>{
+    const routesList = Object.keys(routes).map(function (key, index) {
+      return key;
+    });
+
+    var filteredRoutes = routesList.filter((route) => {
+      return route.indexOf("?") === -1;
+    }); //routes without detail query
+
+    filteredRoutes.forEach((route) => {
+      fetchAndWrite(route);
+    });
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
+
+const fetchAndWrite = (route) => {
+  axios
+    .get(`${baseUrl + route}`)
+    .then((res) => {
+      const data = res.data;
+      const writeData = JSON.stringify(data, null, 2);
+      const params = route.split("/");
+      const fileName = params[params.length - 1];
+      fs.writeFileSync(`${fileName}.result.json`, writeData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
