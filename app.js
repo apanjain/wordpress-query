0d37b0aeccbc590dd3c68aadf660c484333aa6b0
Apan Jain <apanjain3@gmail.com>
2020-12-23 16:33:30 +0530

    write response data to a file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const axios = require("axios");
+const fs = require("fs");
 
 const baseUrl = 'http://0.0.0.0:5000/';
-axios.get(`${baseUrl}?rest_route=/`)
+axios.get(`${baseUrl}?rest_route=/wp/v2/`)
 .then(res=> {
     const data = res.data;
     const routes = data.routes;
-    console.log(routes)
+    const writeData = JSON.stringify(data,null,2);
+    fs.writeFileSync('response.json', writeData);
+
 })
 .catch((err)=>{
     console.log(err);
